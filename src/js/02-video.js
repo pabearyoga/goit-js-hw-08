4d26b714f301d84721760e7704f2ced621a08457
Pabearyoga <pabearyoga@gmail.com>
2022-07-12 17:34:40 +0300

    fix 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+let b = 0;
+player.on('timeupdate', function () {
+  player.getCurrentTime().then(function (seconds) {
+    const a = seconds;
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(a));
+    const b = localStorage.getItem('videoplayer-current-time');
+  });
+});
+
+console.log(b);
+// player.setCurrentTime(b);
