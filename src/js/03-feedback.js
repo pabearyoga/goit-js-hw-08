0fd8529c34b0ec0e9ea7a1f2af9bc7527e481cf3
Pabearyoga <pabearyoga@gmail.com>
2022-07-12 22:37:23 +0300

    fix 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+const y = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+form.addEventListener(
+  'input',
+  throttle(() => {
+    const email = input.value;
+    const message = textarea.value;
+
+    const feedbackFormState = {
+      email: email,
+      message: message,
+    };
+    localStorage.setItem(
+      'feedback-form-state',
+      JSON.stringify(feedbackFormState)
+    );
+  }, 500)
+);
+
+window.addEventListener('load', () => {
+  input.blur();
+
+  if (y === null) {
+    input.value = '';
+    textarea.textContent = '';
+  } else {
+    input.value = y.email;
+    textarea.textContent = y.message;
+  }
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  localStorage.clear();
+  form.reset();
+  textarea.textContent = '';
+  console.log(y);
+});
