9557e1d9ae27ca3924a76a7f4775d48ad46972f9
Pabearyoga <pabearyoga@gmail.com>
2022-07-12 15:09:57 +0300

    fix hw-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,22 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(
+    image =>
+      `<a class="gallery__item" href="${image.original}">
+            <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+        </a>`
+  )
+  .join('');
+gallery.insertAdjacentHTML('beforeend', markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
